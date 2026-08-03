9cf1d172b376af4f49c599640b84468600cc7546
Shadaab Karim <karimshadaab510@gmail.com>
2026-08-03 13:20:16 +0530

    chore: configured pre-commit

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,7 +1,9 @@
+import socket
+
 from decouple import config
 
-from .base import INSTALLED_APPS, MIDDLEWARE
 from .base import *  # noqa: F403
+from .base import INSTALLED_APPS, MIDDLEWARE
 
 # =====================================================================
 # LOCAL CORE SETTINGS
@@ -29,36 +31,33 @@ INTERNAL_IPS = [
     "0.0.0.0",
 ]
 
-import socket
-
 # Debug toolbar config
 hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
 
 INTERNAL_IPS = [ip[:-1] + "1" for ip in ips]
 
 if ENABLE_DEBUG_TOOLBAR:
-
     INSTALLED_APPS.append("debug_toolbar")
-    
+
     MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")
-    
+
     # Debug toolbar panel configurations
     DEBUG_TOOLBAR_PANELS = [
-    'debug_toolbar.panels.history.HistoryPanel',
-    'debug_toolbar.panels.versions.VersionsPanel',
-    'debug_toolbar.panels.timer.TimerPanel',
-    'debug_toolbar.panels.settings.SettingsPanel',
-    'debug_toolbar.panels.headers.HeadersPanel',
-    'debug_toolbar.panels.request.RequestPanel',
-    'debug_toolbar.panels.sql.SQLPanel',
-    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-    'debug_toolbar.panels.templates.TemplatesPanel',
-    'debug_toolbar.panels.alerts.AlertsPanel',
-    'debug_toolbar.panels.cache.CachePanel',
-    'debug_toolbar.panels.signals.SignalsPanel',
-    'debug_toolbar.panels.redirects.RedirectsPanel',
-    'debug_toolbar.panels.profiling.ProfilingPanel',
-]
+        "debug_toolbar.panels.history.HistoryPanel",
+        "debug_toolbar.panels.versions.VersionsPanel",
+        "debug_toolbar.panels.timer.TimerPanel",
+        "debug_toolbar.panels.settings.SettingsPanel",
+        "debug_toolbar.panels.headers.HeadersPanel",
+        "debug_toolbar.panels.request.RequestPanel",
+        "debug_toolbar.panels.sql.SQLPanel",
+        "debug_toolbar.panels.staticfiles.StaticFilesPanel",
+        "debug_toolbar.panels.templates.TemplatesPanel",
+        "debug_toolbar.panels.alerts.AlertsPanel",
+        "debug_toolbar.panels.cache.CachePanel",
+        "debug_toolbar.panels.signals.SignalsPanel",
+        "debug_toolbar.panels.redirects.RedirectsPanel",
+        "debug_toolbar.panels.profiling.ProfilingPanel",
+    ]
 
 # =====================================================================
 # DJANGO-EXTENSIONS SETTINGS
